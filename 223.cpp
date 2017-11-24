409a1365a053fa103bdf6fc31e6f4a81d421c9ea
HanaYukii <islu245777@gmail.com>
2017-11-24 21:24:34 +0800

    Create 223.cpp

---
+++ 223.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+int inc[100001];
+int input()
+{
+    int n = 0;
+    char c;
+    while ((c = getchar()) != ' '&&c != '\n')
+    {
+        if (c == EOF)
+            return -1;
+        n = n * 10 + c - '0';
+    }
+    return n;
+}
+int LIS(vector<int> &v)
+{
+    vector <int> ve;
+    ve.push_back(v[0]);
+    inc[0]=1;
+    for(int i=1;i<v.size();i++)
+    {
+        if(v[i]>ve.back())
+            ve.push_back(v[i]);
+        else
+            *lower_bound(ve.begin(),ve.end(),v[i])=v[i];
+        inc[i]=lower_bound(ve.begin(),ve.end(),v[i])-ve.begin()+1;
+    }
+    return ve.size();
+}
+int main()
+{
+    int m,n;
+    m=input();
+    n=input();
+    int cont[m+1]={0};
+    int cont2[m+1]={0};
+    for(int i=0; i<m; i++)
+    {
+        int add;
+        add=input();
+        cont[add]=i;
+    }
+    for(int t=0; t<n; t++)
+    {
+        vector<int> ans;
+        vector<int> cont2;
+        ans.clear();
+        for(int i=0; i<m; i++)
+        {
+            int add;
+            add=input();
+            cont2.push_back(add);
+            ans.push_back(cont[add]);
+        }
+        int len=LIS(ans);
+        int now=len;
+        int output[len];
+        for(int j=m-1;j>=0;j--)
+        {
+            if(inc[j]==now)
+            {
+                output[--now]=cont2[j];
+            }
+        }
+        for(int i=0;i<len;i++)
+        {
+            printf("%d ",output[i]);
+        }
+        puts("");
+    }
+
+}
