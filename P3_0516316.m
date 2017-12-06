460aa9f4da73ba52806fce9dff56855afff7ac11
HanaYukii <islu245777@gmail.com>
2017-12-07 01:27:58 +0800

    Update P3_0516316.m

--- P3_0516316.m
+++ P3_0516316.m
@@ -1,128 +1,182 @@
-%ID: 0516020 ©m¦W¡G§fº¸°a
+%ID: 0516020 å§“åï¼šå‘‚çˆ¾è»’
 function P3_0516316(path)
-    findjpg = strcat(path, '*.jpg');
-    list = dir(findjpg);
-    num = numel(list);
-    index = 1;
-    
-    if num > 0
-        pic = strcat(path, list(index).name);
-        image = imread(pic);
-        [row, col, dep] = size(image);
-        % resize
-        if row>640 || col>480
-            image = imresize(image, [640 480], 'nearest');
+    pathjpg=strcat(path,'*.jpg');
+    pictures=dir(pathjpg);
+    cnt=numel(pictures);
+    index=1;
+    if cnt>0
+        now=strcat(path, pictures(index).name);
+        image=imread(now);
+        [row,col,h]=size(image);
+        h;
+        if row>=640||col>=480
+            image=imresize(image,[640 480],'nearest');
+            normal=image;
+        else
+            put_row = floor((640-row)/2);
+            put_col = floor((480-col)/2);
+            new_image = padarray(image, [put_row put_col], 0);
+            [row, col, h] = size(new_image);
+            h;
+            nornal = imresize(new_image, [640 480], 'nearest');
         end
-        imshow(image);
-        [row, col, dep] = size(image);
-        title(['Picture ', num2str(index), ' of ', num2str(num), ' (', list(index).name, ') ', num2str(row), 'x', num2str(col)]);
+        imshow(normal);
+        [row,col,h]=size(normal);
+        h;
+        title(['Picture ', num2str(index), ' of ', num2str(cnt), ' (', pictures(index).name, ') ', num2str(row), 'x', num2str(col)]);
     else
         A = zeros(640,480);
         imshow(A);
-        title('No Jpgs');
+        title('No jpgs');
     end
-    while 1 
-        tt1 = waitforbuttonpress;
-        if tt1 ~= 0
-            operator = get(gcf, 'CurrentCharacter');
-            if operator == '<'
-                index = index - 1;
-                if index == 0
-                    index = num;                   
+    
+    while 1
+        t=waitforbuttonpress;
+        if t>0
+            op=get(gcf, 'CurrentCharacter');
+            if op=='<'
+                index=index-1;
+                if index==0
+                    index=cnt;
                 end
-            elseif operator == '>'
-                index = index + 1;
-                if index == num + 1
-                    index = 1;                   
+            elseif op=='>'
+                index=index+1;
+                if index==cnt+1
+                    index=1;
                 end
-            elseif operator == 'q'
+            elseif op=='q'
                 close all;
                 break;
             end
-            pic = strcat(path, list(index).name);
-            image = imread(pic);
-             [row, col, dep] = size(image);
-            if row>640 || col>480
+            now = strcat(path, pictures(index).name);
+            image = imread(now);
+            [row, col, h] = size(image);
+            h;
+            if row>=640 || col>=480
                 image = imresize(image, [640 480], 'nearest');
             end
-            figure('Position',[500,500,100,100]);
-            imshow(image);
-            [row, col, dep] = size(image);
-            title(['Picture ', num2str(index), ' of ', num2str(num), ' (', list(index).name, ') ', num2str(row), 'x', num2str(col)]);
-        % click
+            normal = image;
+            imshow(normal);
+            [row, col, h] = size(image);
+            h;
+            title(['Picture ', num2str(index), ' of ', num2str(cnt), ' (', pictures(index).name, ') ', num2str(row), 'x', num2str(col)]);
         else
-            c = get(gca, 'CurrentPoint');
-            x = c(1,1); % x col
-            y = c(1,2); % y row
+            c1 = get(gca, 'CurrentPoint');
+            x = c1(1,1); % x col
+            y = c1(1,2); % y row
             if x <=480 && x >=1 && y<=640 && y>=1
+                radius = 50; 
+                figure(1);
+                imshow(normal);
+                top=min(y+radius,480);
+                bot=max(0,y-radius);
+                left=max(0,x-radius);
+                right=min(640,x+radius);
+                line([left right], [top top], 'color', 'k', 'LineWidth', 3);
+                line([left right], [bot bot], 'color', 'k', 'LineWidth', 3);
+                line([left left], [bot top], 'color', 'k', 'LineWidth', 3);
+                line([right right], [bot top], 'color', 'k', 'LineWidth', 3);
                 figure('Position',[1400,500,100,100]);
                 imshow(image);
-                leng = 50; % initial
-                axis([x-leng x+leng y-leng y+leng]);
+                am = ((2*radius)*(2*radius)) / (row*col) *100;
+                title(['Picture ', num2str(index), ' of ', num2str(cnt), ' (', pictures(index).name, ') ', num2str(row), 'x', num2str(col), ' @ ', num2str(am), ' %']);
+                axis([x-radius x+radius y-radius y+radius]);
                 while 1
                     tt2 = waitforbuttonpress;
                     if tt2 ~= 0
                         operator2 = get(gcf, 'CurrentCharacter'); 
                         if operator2 == '+'
-                            leng = leng * 1.1;
-                            if leng>200
-                                leng=200;
+                            radius = radius * 0.91;
+                            if radius < 10
+                                radius = 10;
                             end
                         elseif operator2 == '-'
-                            leng = leng * 0.9;
-                            if leng<10
-                                leng=10;
+                            radius = radius * 1.09;
+                            if radius > 150
+                                radius = 150;
                             end
-                        elseif operator2 == 28
-                            x=x-10;
-                            if(x<1)
-                                x=1;
-                            end
-                        elseif operator2 == 29
-                            x=x+10;
-                            if(x>480)
-                                x=480;
-                            end
-                        elseif operator2 == 31
-                            y=y+10;
-                            if(y>640)
-                                y=640;
-                            end
-                        elseif operator2 == 30
-                            y=y-10;
-                            if(y<1)
-                                y=1;
-                            end  
-                        elseif operator2 == '>' || operator == '<'
+                        elseif operator2 == '>' || operator2 == '<' || operator2 == 'q'
                             close(figure(2));
                             if operator2 == '>'
                                 index = index + 1;
-                                if index == num + 1
+                                if index == cnt + 1
                                     index = 1;
                                 end
-                            else
-                                index = index -1;
+                            elseif operator2 == '<'
+                                index = index - 1;
                                 if index < 1
-                                    index = num;
+                                    index = cnt;
                                 end
                             end
-                            str = strcat(path, list(index).name);
+                            str = strcat(path, pictures(index).name);
                             image = imread(str);
-                             [row, col, dep] = size(image);
+                            [row, col, h] = size(image);
+                            h;
                             if row>640 || col>480
                                 image = imresize(image, [640 480], 'nearest');
+                                normal = image;
+                            else
+                                normal = image;
                             end
-                            imshow(image);
-                            [row, col, dep] = size(image);
-                            title(['Picture ', num2str(index), ' of ', num2str(num), ' (', list(index).name, ') ', num2str(row), 'x', num2str(col)]);
+                            imshow(normal);
+                            [row, col, h] = size(normal);
+                            h;
+                            title(['Picture ', num2str(index), ' of ', num2str(cnt), ' (', pictures(index).name, ') ', num2str(row), 'x', num2str(col)]);
+                            break;
+                        %arrow----
+                        elseif operator2 == 28 
+                            x = x - 10;
+                            if x < 1
+                                x = 1;
+                            end
+                        elseif operator2 == 29 
+                            x = x + 10;
+                            if x > 480
+                                x = 480;
+                            end
+                        elseif operator2 == 31 
+                            y = y + 10;
+                            if y > 640
+                                y = 640;
+                            end
+                        elseif operator2 == 30 
+                            y = y - 10;
+                            if y < 1
+                                y = 1;
+                            end
+                        end
+                        figure(1);
+                        imshow(normal);
+                        line([x-radius x+radius], [y-radius y-radius], 'color', 'k', 'LineWidth', 3);
+                        line([x-radius x+radius], [y+radius y+radius], 'color', 'k', 'LineWidth', 3);
+                        line([x-radius x-radius], [y-radius y+radius], 'color', 'k', 'LineWidth', 3);
+                        line([x+radius x+radius], [y-radius y+radius], 'color', 'k', 'LineWidth', 3);
+                        figure(2);
+                        am = ((2*radius)*(2*radius)) / (row*col) * 100;
+                        imshow(image);
+                        title(['Picture ', num2str(index), ' of ', num2str(cnt), ' (', pictures(index).name, ') ', num2str(row), 'x', num2str(col), ' @ ', num2str(am), ' %']);
+                        axis([x-radius x+radius y-radius y+radius]);
+                    else
+                        c2 = get(gca, 'CurrentPoint');
+                        x2 = c2(1, 1);
+                        y2 = c2(1, 2);
+                        if x2 < x-radius || x2 > x+radius || y2 < y-radius || y2 > y+radius
+                            close (figure(2));
+                            figure(1);
+                            imshow(normal);
+                            [row, col, h] = size(normal);
+                            h;
+                            title(['Picture ', num2str(index), ' of ', num2str(cnt), ' (', pictures(index).name, ') ', num2str(row), 'x', num2str(col)]);
                             break;
                         end
-                        imshow(image);
-                        axis([x-leng x+leng y-leng y+leng]);
-                        axis square;
-                    end
+                    end % if tt2
                 end % while end for tt2
             end
         end
     end %while end for tt1
 end
+            
+            
+            
+            
+            
